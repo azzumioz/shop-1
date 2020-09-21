a1cc7243f1642bbb1bc74f10d4df04a4faa718b2
azzumioz <azzumioz@gmail.com>
2020-09-22 00:21:41 +0700

    +create component CommonPage for Contacts, Delivery, Guarantee pages *add routes for this pages

--- src/server/route/root_route.js
+++ src/server/route/root_route.js
@@ -6,5 +6,8 @@ const {
 } = require ('../controller/root_controller');
 
 router.get('/', serveSPA);
+router.get('/delivery', serveSPA);
+router.get('/guarantee', serveSPA);
+router.get('/contacts', serveSPA);
 
 module.exports = router;
